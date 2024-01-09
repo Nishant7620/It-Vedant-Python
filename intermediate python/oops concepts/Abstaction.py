69eeeaa8f7d0ced5d0abd5b7c20880b417925165
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-09 11:30:30 +0530

    updated

---
+++ intermediate python/oops concepts/Abstaction.py
@@ -0,0 +1,14 @@
+class Cirlce:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+    def perimeter(self):
+        return 2 * 3.14 *self.radius    
+
+calc = Cirlce(8)
+print(calc.area())
+print(calc.perimeter())
+
