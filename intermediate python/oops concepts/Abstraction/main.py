9064096b6f70d9b5303a4cb733fc929f2e21fae6
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-10 10:06:16 +0530

    updated

--- intermediate python/oops concepts/Abstraction/main.py
+++ intermediate python/oops concepts/Abstraction/main.py
@@ -5,3 +5,8 @@ c = Circle(5)
 
 print(f"Area of a cirlce: {c.area()}")
 print(f"Perimeter of a circle: {c.perimeter()}")
+
+r =  Rectangle(5,3)
+
+print(f"Area of a rectangle: {r.area()}")
+print(f"Perimeter of a rectangle: {r.perimeter()}")
