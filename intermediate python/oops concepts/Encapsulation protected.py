195e654551ffa174a7cce0e9603c9c256fb11ed5
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-10 10:30:16 +0530

    encapsulation (protected)

---
+++ intermediate python/oops concepts/Encapsulation protected.py
@@ -0,0 +1,22 @@
+class Vehicle:
+
+    def __init__(self,color,wheels):
+        self._color = color
+        self._wheels = wheels
+
+class Car(Vehicle):
+
+    def __init__(self,color,wheels,doors):
+        super().__init__(color,wheels)
+        self._doors = doors  
+
+    def car_details(self):
+        print(f"color of car {self._color}, No. of {self._wheels}, No.of doors {self._doors}")          
+
+car = Car("black",4,4)
+car.car_details()
+
+#Accessing protected variable using object
+print(f"Before changing protected variable of wheel: {car._wheels}")
+car._wheels = 10
+print(f"After changing protected variable of wheel: {car._wheels}")
