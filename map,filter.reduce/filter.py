446a86f9c39e76f1d4e479de1c9caf682211937e
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-15 11:59:09 +0530

    updated

--- map,filter.reduce/filter.py
+++ map,filter.reduce/filter.py
@@ -18,3 +18,20 @@ even_number = filter(lambda x:x%2==0,numbers)
 
 even_list = list(even_number)
 print(even_list)
+
+#-----------------------------------------------------------------
+#checking string empty or not
+
+string = ["hello" ,"","world"]
+
+check_string = list(filter(lambda s: len(s)>0,string))
+print(check_string)
+
+#-----------------------------------------------------------------
+#using null in filter
+
+numbers =[1,2,3,4,4,5,6,7,8,9,10]
+numbers = [True,False,True,False,True]
+
+even_numbers = list(filter(None,numbers))
+print(even_numbers)
