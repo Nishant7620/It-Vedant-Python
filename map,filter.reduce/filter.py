30f902a5318cdac40c0da3e2d1577fc55511b47c
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-15 11:42:35 +0530

    updated

--- map,filter.reduce/filter.py
+++ map,filter.reduce/filter.py
@@ -1,3 +1,4 @@
+"""
 numbers = [1,2,3,4,5]
 
 def is_even(n):
@@ -7,5 +8,13 @@ even = filter(is_even,numbers)
 list =list(even)
 
 print(list)
-
+"""
 #-----------------------------------------------------------------
+#applying filter () in lambda function
+
+numbers = [1,2,3,4,5,6,7,8]
+
+even_number = filter(lambda x:x%2==0,numbers)
+
+even_list = list(even_number)
+print(even_list)
