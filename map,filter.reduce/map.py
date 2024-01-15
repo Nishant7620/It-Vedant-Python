c8b000abbd0fa116dfe9cf71bd92782159fd9340
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-15 11:21:27 +0530

    updated

--- map,filter.reduce/map.py
+++ map,filter.reduce/map.py
@@ -22,3 +22,19 @@ print(result)
 
 #------------------------------------------------------------------------
 #squaring a number using lambda function in map() function
+"""
+numbers = [1,2,3,4,5]
+doubled_numbers = map(lambda x:x*2,numbers)
+list =list(doubled_numbers)
+print(list)
+"""
+#--------------------------------------------------------------------------
+#square a number using lambda function in map() function with two argument
+
+number1 = [1,2,3,4,5]
+number2 = [10,20,30,40,50]
+
+two_argument = map(lambda x,y:x*y,number1,number2)
+
+result = list(two_argument)
+print(result)
