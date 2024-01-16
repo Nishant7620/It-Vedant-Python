c44a153cc132b722d7212c98370e3c229995f45f
Nishant7620 <nkaranjavkar72@gmail.com>
2024-01-16 11:15:42 +0530

    updated

---
+++ decorator function/decorator.py
@@ -0,0 +1,13 @@
+def make_pretty(func):
+    def inner ():
+        print("i got decorated")
+        func()
+    return inner
+
+def ordinary():
+    print("i am ordinary")
+
+result = make_pretty(ordinary)
+result()
+
+
